7a76c1e38b4f6e3d91ef1692e37c113140f0f4bd
Carlos Antunis <83259459+Carlos-Antunis@users.noreply.github.com>
2022-04-23 14:08:53 -0300

    Update crivo.f90

--- IFC/1.3/programas/crivo.f90
+++ IFC/1.3/programas/crivo.f90
@@ -5,17 +5,17 @@ PROGRAM Crivo_de_Eratostenes
 	LOGICAL :: E_Primo(2:100) =.TRUE.
 	integer :: I
 
-  !Para todos os números entre 2 e 100 que são primos
+	!Para todos os números entre 2 e 100 que são primos
 	FORALL (I=2:100, E_Primo(i))
-    !Eliminar seus múltiplos
-    E_Primo(2*I:100:I) = .FALSE.
-  ENDFORALL
+		!Eliminar seus múltiplos
+		E_Primo(2*I:100:I) = .FALSE.
+	ENDFORALL
 
-  !Retornar todos os números entre 2 e 100 que são primos 
+	!Retornar todos os números entre 2 e 100 que são primos 
 	DO I=2,100
 		IF (E_Primo(i)) THEN
-      PRINT*, i
-    ENDIF
+			PRINT*, i
+		ENDIF
 	ENDDO
 
 END PROGRAM Crivo_de_Eratostenes
